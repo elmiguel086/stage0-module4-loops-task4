7cef73d4ca41f7c2fcef54df0f86af15b1c6b1e6
elmiguel0 <miguelangel.ramos@ibm.com>
2022-11-30 21:11:20 -0600

    changes done

--- src/main/java/school/mjc/stage0/loops/task4/SkippingThird.java
+++ src/main/java/school/mjc/stage0/loops/task4/SkippingThird.java
@@ -2,6 +2,13 @@ package school.mjc.stage0.loops.task4;
 
 public class SkippingThird {
     public void printUntilButThird(int lastPrinted) {
-
+        int skip = 3;
+        for (int i = 1; i <= lastPrinted; i++) {
+            if(i == skip){
+                skip += skip;
+                continue;
+            }
+            System.out.println(i);
+        }
     }
 }
